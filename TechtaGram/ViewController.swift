d4277ede0680e2c4b127c0abc2c8c408ac48bff2
Marina <marinahara0223@yahoo.co.jp>
2022-08-28 01:42:22 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by 原田摩利奈 on 8/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var cameraImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func takePhoto(){
+        if UIImagePickerController.isSourceTypeAvailable( .camera) {
+            //カメラ起動
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            
+            picker.allowsEditing = true
+            
+            present(picker, animated: true, completion: nil)
+        } else {
+            print("error")
+        }
+    }
+    
+    @IBAction func savePhoto(){
+        
+    }
+    
+    @IBAction func openAlbum(){
+        
+    }
+    
+    @IBAction func snsPhoto(){
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        cameraImageView.image = info[.editedImage] as? UIImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //p.20からだよ
+
+}
+
